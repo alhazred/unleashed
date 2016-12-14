citrus-it/illumos-omnios

commit 71d2275dbeb43b8451c87448d3b1045ef9d1abc4

kill tsol ("Trusted Solaris") aka TX ("Trusted Extensions")

usr/src/cmd/fs.d/nfs/statd/sm_svc.c
@@ -718,7 +718,6 @@ static void
 set_statmon_owner(void)
 {
 	int i;
-	boolean_t can_do_mlp;
 
 	/*
 	 * Recursively chown/chgrp /var/statmon and the alternate paths,
@@ -732,9 +731,7 @@ set_statmon_owner(void)
 		one_statmon_owner(alt_path);
 	}
 
-	can_do_mlp = priv_ineffect(PRIV_NET_BINDMLP);
-	if (__init_daemon_priv(PU_RESETGROUPS|PU_CLEARLIMITSET,
-	    daemon_uid, daemon_gid, can_do_mlp ? PRIV_NET_BINDMLP : NULL,
+	if (__init_daemon_priv(PU_RESETGROUPS|PU_CLEARLIMITSET, daemon_uid,
 	    NULL) == -1) {
 		syslog(LOG_ERR, "can't run unprivileged: %m");
 		exit(1);

alhazred/unleashed

commit 69393959e0256c03b92f5dab48f942087a8a2a91

statd: fix __init_daemon_priv
Commit 71d2275dbeb43b8451c87448d3b1045ef9d1abc4 accidentally removed the
gid argument from this call, breaking statd.

usr/src/cmd/fs.d/nfs/statd/sm_svc.c
@@ -732,7 +732,7 @@ set_statmon_owner(void)
 	}
 
 	if (__init_daemon_priv(PU_RESETGROUPS|PU_CLEARLIMITSET, daemon_uid,
-	    NULL) == -1) {
+	    daemon_gid, NULL) == -1) {
 		syslog(LOG_ERR, "can't run unprivileged: %m");
 		exit(1);
 	}